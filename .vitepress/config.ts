6966a7a7aec8dbe67e6d6e9fb05c4cd05edca270
Samuel Okolo <herookolo@gmail.com>
2026-06-05 14:52:04 +0100

    fix: vercel build issue

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Flowstride",
   description: "An open source flow first test automation framework",
+
+  ignoreDeadLinks: true,
+
   head: [["link", { rel: "icon", href: "/public/favicon.ico" }]],
   themeConfig: {
     logo: "/logo.png",
